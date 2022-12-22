37e5517f603b10659dcb94982a13258ddad779c5
Melissa Farrell <caretcake@outlook.com>
2022-12-22 17:25:53 -0500

    Solve day four part two

--- dayfour.py
+++ dayfour.py
@@ -9,9 +9,10 @@ for line in file:
     assignments = ' '.join(line.split()).split(',')
     assignmentOne = assignments[0].split('-')
     assignmentTwo = assignments[1].split('-')
-    if int(assignmentOne[0]) >= int(assignmentTwo[0]) and int(assignmentOne[1]) <= int(assignmentTwo[1]):
-        totalOverlapCount += 1
-    elif int(assignmentOne[0]) <= int(assignmentTwo[0]) and int(assignmentOne[1]) >= int(assignmentTwo[1]):
+    if ((int(assignmentOne[0]) >= int(assignmentTwo[0]) and int(assignmentOne[0]) <= int(assignmentTwo[1])) or
+        (int(assignmentOne[1]) >= int(assignmentTwo[0]) and int(assignmentOne[1]) <= int(assignmentTwo[1])) or
+        (int(assignmentTwo[0]) >= int(assignmentOne[0]) and int(assignmentTwo[0]) <= int(assignmentOne[1])) or
+        (int(assignmentTwo[0]) >= int(assignmentOne[0]) and int(assignmentTwo[0]) <= int(assignmentOne[1]))):
         totalOverlapCount += 1
 
 print(totalOverlapCount)
