24131c5c3d26fa5f3bc51f23ff902279e92f11bb
Melissa Farrell <caretcake@outlook.com>
2022-12-24 20:45:51 -0500

    Solve day six part two

--- daysix.py
+++ daysix.py
@@ -1,21 +1,26 @@
 import time
 def main():
+    print_marker(4)
+    print_marker(14)
+
+
+
+def print_marker(markerLength):
     file = open("input.txt", "r")
     left = 0
     right = 1
     for line in file:
         while right < len(line):
-            if right < 4:
+            if right < markerLength:
                 right += 1
                 continue
             potentialMarker = line[left:right]
             markerSet = set(potentialMarker)
-            if len(markerSet) == 4:
+            if len(markerSet) == markerLength:
                 print(f"{potentialMarker} : {right}")
                 break
             left += 1
             right += 1
-
     file.close()
 
 if __name__ == '__main__':
